9605d89c92b66657a6d886c60bc5016e7e80fbca
tuan <minhtuanto179@gmail.com>
2026-07-27 13:13:56 +0700

    [seed]: update

--- seed_service/src/main/java/com/miniecommerce/seed/SeedServiceApplication.java
+++ seed_service/src/main/java/com/miniecommerce/seed/SeedServiceApplication.java
@@ -2,12 +2,10 @@ package com.miniecommerce.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
         LiquibaseAutoConfiguration.class
 })
 @EnableConfigurationProperties(SeedTargetsProperties.class)
