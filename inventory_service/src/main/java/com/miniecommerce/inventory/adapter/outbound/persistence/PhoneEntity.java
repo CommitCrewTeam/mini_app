2d6fda00cffa6435050b512dcea9094006872cb8
tuan <minhtuanto179@gmail.com>
2026-08-28 23:22:16 +0700

    [order]:api send to kafka

---
+++ inventory_service/src/main/java/com/miniecommerce/inventory/adapter/outbound/persistence/PhoneEntity.java
@@ -0,0 +1,67 @@
+package com.miniecommerce.inventory.adapter.outbound.persistence;
+
+import io.r2dbc.postgresql.codec.Json;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("phones")
+public class PhoneEntity {
+
+    @Id
+    private Long id;
+    private String name;
+    private Json detail;
+    private boolean active;
+    private int stock;
+
+    public PhoneEntity() {
+    }
+
+    public PhoneEntity(Long id, String name, Json detail, boolean active, int stock) {
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
+        this.active = active;
+        this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Json getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Json detail) {
+        this.detail = detail;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
